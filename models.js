ac5d191d2f456cc9431494c9a98465c995fa172e
Sarorian <shamowen5@gmail.com>
2023-04-30 19:57:09 -0600

    Update models.js

--- models.js
+++ models.js
@@ -40,7 +40,7 @@ userSchema.statics.hashPassword = (password) => {
 }
 
 userSchema.methods.validatePassword = function(password) {
-    return bcrypt.compareSync(password, this.password);
+    return bcrypt.compareSync(password, this.Password);
 
 }
 

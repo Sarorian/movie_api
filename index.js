7cd947f1dc9a0aa40dc51bbcbf26ead8d7076823
Sarorian <shamowen5@gmail.com>
2023-05-02 00:16:02 -0600

    updated cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const { check, validationResult } = require('express-validator');
 
 app.use(bodyParser.json());
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com']
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234']
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
